d7dcfe76e04adf8bc858b4c74f3432cf5faec206
Guilherme Lourenço <aluno23053@ipt.pt>
2022-05-31 17:26:20 +0100

    Adição de comnetários ao código base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+/**
+ * App.js
+ */
+
+//importação de recursos
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Criação de um componente. Neste caso, chamar-se-á App.
+ * Os componentes começam SEMPRE por uma maiúscula.
+ * 
+ * Os componentes podem ser de dois tipos:
+ *   - classes
+ *   - funções
+ * 
+ * Em todos os componentes haverá sempre um RETURN
+ * @returns 
+ */
 function App() {
   return (
     <div className="App">
+      {/*Este é um comentário...
+         Eles mudam conforme o contexto.
+         Neste sítio estamos a escrever JSX, não HTML*/}
       <header className="App-header">
+        {/*O termo 'classNAme' existe para não entrar em conflito com o termo 'class'.
+           Em 'run time', será convertido em 'class' no browser*/}
         <img src={logo} className="App-logo" alt="logo" />
+        {/*O acesso aos dados de um recurso, iremos colocá-lo entre {}*/}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +42,8 @@ function App() {
       </header>
     </div>
   );
-}
+} //fim do componente
 
+// um 'componente' só será acessível a outros componentes se for exportado
 export default App;
+
